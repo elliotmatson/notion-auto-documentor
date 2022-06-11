d13ae46141ca3756d640e03c0afd3dd474acac94
Elliot Matson <ematson5897@gmail.com>
2022-06-11 17:18:14 -0500

    Switch to Go

---
+++ Testing/notion.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	notion "github.com/dstotijn/go-notion"
+)
+
+const apiKey = "secret_L4Fbaz5krjoU9vtDcgeZ0V9Blf3N6pbIj1SewPZHDws"
+
+func main() {
+	client := notion.NewClient(apiKey)
+	//http.HandleFunc("/", handler)
+
+	//http.ListenAndServe(":8080", nil)
+
+	results, err := client.Search(context.Background(), &notion.SearchOpts{ /*Query: "test", */ Filter: &notion.SearchFilter{Value: "database", Property: "object"}})
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	for i := 0; i < len(results.Results); i++ {
+		switch t := results.Results[i].(type) {
+		case notion.Database:
+			fmt.Printf("Page: %v\n", t.Title[0].PlainText)
+			//result, _ := client.QueryDatabase(context.Background(), t.ID, &notion.DatabaseQuery{})
+			initDB(client, t.ID)
+			//spew.Dump(result)
+		default:
+			fmt.Printf("wrong type %T\n", t)
+		}
+	}
+}
+
+// sets up a notion db with the required parameters
+func initDB(c *notion.Client, db string) {
+	t := []notion.RichText{{Text: &notion.Text{Content: "test"}}}
+	p := make(notion.DatabasePageProperties)
+	p["Name"] = notion.DatabasePageProperty{Title: t}
+	p["Poops"] = notion.DatabasePageProperty{Title: []notion.RichText{{Text: &notion.Text{Content: "poop poop"}}}}
+
+	_, err := c.UpdateDatabase(context.Background(), db, notion.UpdateDatabaseParams{})
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+}
+
+// adds a page to a notion db
+func addPage(c *notion.Client, db string, title string) {
+	t := []notion.RichText{{Text: &notion.Text{Content: title}}}
+	p := make(notion.DatabasePageProperties)
+	p["Name"] = notion.DatabasePageProperty{Title: t}
+	_, err := c.CreatePage(context.Background(), notion.CreatePageParams{ParentType: notion.ParentTypeDatabase, ParentID: db, DatabasePageProperties: &p})
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/notion-auto-documentor
+
+go 1.18
+
+require github.com/dstotijn/go-notion v0.6.1
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
